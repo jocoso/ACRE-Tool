470790fb6670e8d28d49fda709c861836e5c0387
jocoso <joshua.collado022@gmail.com>
2024-12-09 22:54:34 -0500

    General Bug Fixes

--- nn3.py
+++ nn3.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 # Core Engine
 class GameEngine:
+
     def __init__(self):
         self.modules = []  # List of modules, such as input handling or game logic
         self.running = True  # Engine running flag
@@ -35,6 +36,7 @@ class GameEngine:
 
 # Abstract Module Base Class
 class Module(ABC):
+
     @abstractmethod
     def execute(self):
         """Executes module logic."""
@@ -43,9 +45,10 @@ class Module(ABC):
 
 # Abstract Piece Base Class
 class Piece(ABC):
+
     def __init__(self, name, description):
-        self.name = name
-        self.description = description
+        self.attributes = { 'name': name, 'description': description}
+
     @abstractmethod
     def update(self):
         """Update piece state."""
@@ -54,6 +57,7 @@ class Piece(ABC):
 
 # Player Piece
 class PlayerPiece(Piece):
+
     def __init__(self):
         super().__init__("Player", "The main character of the game.")
         self.data = {}
@@ -65,6 +69,7 @@ class PlayerPiece(Piece):
 
 # Input Mapping Module
 class InputMapModule(Module):
+
     def __init__(self):
         self.commands = {}  # Dictionary to store commands by trigger
 
@@ -75,7 +80,7 @@ class InputMapModule(Module):
         else:
             raise TypeError("Only instances of Command can be added as commands.")
     
-    def process_command(self, input):
+    def process_command(self, user_input):
         command = self.commands.get(user_input)
         if command:
             command.execute()
@@ -89,6 +94,7 @@ class InputMapModule(Module):
 
 # Abstract Command Base Class
 class Command(Piece):
+
     def __init__(self, trigger):
         self.trigger = trigger  # Command trigger word or phrase
 
@@ -104,6 +110,7 @@ class Command(Piece):
 
 # Quit Command
 class QuitCommand(Command):
+
     def __init__(self, engine):
         super().__init__('quit')
         self.engine = engine
@@ -115,17 +122,22 @@ class QuitCommand(Command):
 
 # Room Piece
 class RoomPiece(Piece):
+
     def __init__(self, room_name, description):
         super().__init__(room_name, description)
+
     def describe_room(self):
         print(f"You are in {self.name}. {self.description}")
+
     def update(self):
         self.describe_room()
 
 
 # Map Grid Module
 class MapGridModule(Module):
+
     class Vector:
+
         def __init__(self, x=None, y=None):
             self.x = x
             self.y = y
@@ -134,7 +146,7 @@ class MapGridModule(Module):
             return self.x is not None and self.y is not None
 
     def __init__(self, size_x, size_y):
-        self.size_vector = Vector(size_x, size_y)
+        self.size_vector = self.Vector(size_x, size_y)
         self.map_grid = [[None for _ in range(size_x)] for _ in range(size_y)]
         
     def add_to_first_empty_space(self, element):
@@ -150,7 +162,7 @@ class MapGridModule(Module):
         if location and location.is_valid():
             if self.map_grid[location.y][location.x] is None:
                 self.map_grid[location.y][location.x] = room_piece
-                print(f"Room '{room_piece.name}' added at ({location.x}, {location.y}).")
+                print(f"Room '{room_piece.attributes['name']}' added at ({location.x}, {location.y}).")
             else:
                 print(f"Location ({location.x}, {location.y}) is already occupied.")
         else:
@@ -160,9 +172,9 @@ class MapGridModule(Module):
     def remove_room(self, location):
         if location and location.is_valid():
             if self.map_grid[location.y][location.x] is not None:
+                room_piece = self.map_grid[location.y][location.x]
                 self.map_grid[location.y][location.x] = None
-                print(f"Room '{room_piece.name}' removed.")
-            
+                print(f"Room '{room_piece.attributes['name']}' removed.")
 
     def execute(self):
         """Display the map grid."""
