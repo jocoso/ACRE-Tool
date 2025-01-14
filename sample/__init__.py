c578abc9227224af20e51da292ff2355f0a90d2c
a <a@jocoso-right.lan>
2025-01-14 12:18:02 -0500

    Organized files and packages according to Python bizarre ways.

---
+++ sample/__init__.py
@@ -0,0 +1,4 @@
+from . import PropManager
+from . import StageManager
+
+__all__ = ["PropManager", "StageManager"]
---
+++ sample/core.py
@@ -0,0 +1,9 @@
+# ACRE Tool
+# by Joshua Collado
+
+from managers import StageManager, PropManager
+
+if __name__ == '__main__':
+    sm = StageManager()
+    pm = PropManager()
+    print("The code has been successfully executed! Bye.")
